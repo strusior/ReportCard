c6f02d7babc242bca46229c38d30a16c3b38732b
strusior <strusior@gmail.com>
2017-06-03 15:55:59 +0200

    2nd iteration of udacity reportcard project

--- app/src/main/java/com/example/android/reportcard/ReportCard.java
+++ app/src/main/java/com/example/android/reportcard/ReportCard.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 public class ReportCard {
 
     //students names
-    private String adam = "Adam";
-    private String mark = "Mark";
-    private String steven = "Steven";
+    final private String adam = "Adam";
+    final private String mark = "Mark";
+    final private String steven = "Steven";
 
     //stores classes of adam
     private ArrayList<String> adamsClasses = new ArrayList<String>();
@@ -16,49 +16,59 @@ public class ReportCard {
     //don't know why "add" does not work
     //it works when I use "protected void onCreate(Bundle savedInstanceState) {
     //super.onCreate(savedInstanceState);" but then other things don't work :-/
-    adamsClasses.add(0, "history");
-    adamsClasses.add(1, "math");
-    adamsClasses.add(2, "biology");
 
+    public void addAdamClasses (){
+        adamsClasses.add(0, "history");
+        adamsClasses.add(1, "math");
+        adamsClasses.add(2, "biology");}
     //stores classes of mark
     private ArrayList<String> marksClasses = new ArrayList<String>();
 
+
     //allows to add classes to marks list of classes
+    public void addMarkClasses () {
     marksClasses.add(0, "history");
     marksClasses.add(1, "math");
-    marksClasses.add(2, "physics");
+    marksClasses.add(2, "physics");}
 
     //stores classes of steven
     private ArrayList<String> stevensClasses = new ArrayList<String>();
 
+
     //allows to add classes to stevens list of classes
+    public void addStevenClases (){
     stevensClasses.add(0, "history");
     stevensClasses.add(1, "math");
-    stevensClasses.add(2, "english");
+    stevensClasses.add(2, "english");}
 
     //stores grades of Adam from each classes
     private ArrayList<Integer> adamsGradesInHistory = new ArrayList<Integer>();
 
     //allows to add grades
+
+    public void addAdamGradesInHistory (){
     adamsGradesInHistory.add(0, 2);
     adamsGradesInHistory.add(1, 2);
     adamsGradesInHistory.add(2, 4);
-    adamsGradesInHistory.add(3, 3);
+    adamsGradesInHistory.add(3, 3);}
 
     ArrayList<Integer> adamsGradesInMath = new ArrayList<Integer>();
+    public void addAdamGradesInMath (){
     adamsGradesInMath.add(0, 5);
     adamsGradesInMath.add(1, 5);
     adamsGradesInMath.add(2, 5);
-    adamsGradesInMath.add(3, 5);
+    adamsGradesInMath.add(3, 5);}
 
     ArrayList<Integer> adamsGradesInBiology = new ArrayList<Integer>();
+
+    public void addAdamGradesInBiology (){
     adamsGradesInBiology.add(0, 3);
     adamsGradesInBiology.add(1, 2);
     adamsGradesInBiology.add(2, 4);
-    adamsGradesInBiology.add(3, 4);
+    adamsGradesInBiology.add(3, 4);}
 
     // gets Classes of selected student
-    private static String getClasses (ArrayList<String> arrayList){
+    public String getClasses (ArrayList<String> arrayList){
         String courses = null;
         for (int i = 0; i < arrayList.size(); i++) {
             arrayList.get(i);
@@ -68,20 +78,27 @@ public class ReportCard {
     }
 
     //gets grades of selected student from selected classes
-    private static String getGrades (ArrayList<Integer> arrayList, String studentName){
+    public String getGrades (ArrayList<Integer> arrayList, String studentName){
         String grades = null;
         for (int j = 0; j < arrayList.size(); j++) {
-            grades = grades + " " + String.valueOf(arrayList.get(j)); //instead of toString()
+            grades = grades + " " + String.valueOf(arrayList.get(j));
         }
         return studentName + " grades are: " + grades;
     }
 
+    //toString method for Adams grades in math
+    @Override //what would happen if I didn't write "@Override"?
+    public  String toString (){
+
+        return getGrades(adamsGradesInMath, adam);
+    }
+
     //replaces one classes name in a specific list of classes
-    private static void setClasses (ArrayList<String> arrayList, String nameOfClasses, int index){
+    public void setClasses (ArrayList<String> arrayList, String nameOfClasses, int index){
         arrayList.set(index, nameOfClasses);
     }
 
-    //constructor - is it ok? what could it be used for? I would appreciate some example
+    //constructor
     public ReportCard (String adam,
                        String steven,
                        String mark,
@@ -91,9 +108,7 @@ public class ReportCard {
                        ArrayList<Integer> adamsGradesInHistory,
                        ArrayList<Integer> adamsGradesInMath,
                        ArrayList<Integer> adamsGradesInBiology){
-        this.adam = adam;
-        this.steven = steven;
-        this.mark = mark;
+
         this.adamsClasses = adamsClasses;
         this.stevensClasses = stevensClasses;
         this.marksClasses = marksClasses;
